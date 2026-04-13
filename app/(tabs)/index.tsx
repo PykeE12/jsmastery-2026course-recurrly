5444e35d728ab90d1ae03059b29cdb2d760acc86
Your Name <your-email@example.com>
2026-04-13 13:08:15 +0200

    Initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,44 @@
+import "@/global.css";
+import { Link } from "expo-router";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
+
+export default function App() {
+  return (
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+
+      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
+        Go to OnBoarding
+      </Link>
+
+      <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">
+        Go to Sign In
+      </Link>
+
+      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">
+        Go to Sign Up
+      </Link>
+
+      <Link href="/subscriptions/spotify" className="mt-4 rounded bg-primary text-white p-4">
+      Spotify Subscriptions
+      </Link>
+
+      <Link
+        href={{
+          pathname: "/subscriptions/[id]",
+          params: { id: "claude" },
+          }}
+        >
+        Claude Max Subscription
+      </Link>
+
+      
+    </SafeAreaView>
+  );
+}
